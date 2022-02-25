5f073e5d7249a51b3ff2fe54c99d9d7767e32b26
NasifTazwar <nasiftazwar@gmail.com>
2022-02-25 19:23:53 +0600

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,109 @@
-import React,{ChangeEvent, FC, useState} from 'react';
+import React,{ChangeEvent, FC, useCallback, useReducer, useRef, useState} from 'react';
 import './App.css';
+
+interface Todo{
+  id:number;
+  homeWorkName: string;
+  daysToComplete: string;
+}
+
+type ActionType =
+  | { type: "ADD"; homeWorkName: string, daysToComplete: string }
+  | { type: "REMOVE"; id: number };
 
 const App : FC = () => {
 
-  const [homeWork, setHomeWork] = useState<string>("");
-  const [days, setDays] = useState<number>(0);
-  const [todoList, setTodoList] = useState([]);
+  // const [myState, setMyState] = useState<Todo>();
 
-
-  const handleOnChange = (e : ChangeEvent<HTMLInputElement>) : void =>{
-    if(e.target.value === "homeWork"){
-      setHomeWork(e.target.value);
+  
+  function reducer(state: Todo[], action: ActionType) {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            homeWorkName: action.homeWorkName,
+            daysToComplete: action.daysToComplete,
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
     }
-    else{
-      setDays(parseInt(e.target.value));
-    }
-  };
-
-  const addHomeWork = () : void =>{
-    setTodoList([...todoList , homeWork])
   }
+
+  const [todoLists, dispatch] = useReducer(reducer, []);
+
+  const newInputTodo = useRef<HTMLInputElement>(null);
+  const newInputTodo2 = useRef<HTMLInputElement>(null);
+
+  const AddInputTodo = useCallback(() => {
+    if (newInputTodo.current && newInputTodo2.current) {
+      dispatch({
+        type: "ADD",
+        homeWorkName: newInputTodo.current.value,
+        daysToComplete: newInputTodo2.current.value,
+      });
+      newInputTodo.current.value = "";
+      newInputTodo2.current.value = "";
+    };
+    // console.log(sta);
+  }, []);
 
   return (
     <div className="App container-fluid d-flex align-items-center flex-column">
       <div className="header mt-5 d-flex justify-content-center">
-        <div className='container my-5 g-2 row align-items-center w-75'>
-          <input className='form-control col-12' type="text" placeholder='Homeworks...' name='homeWork' onChange={handleOnChange}/>
-          <input className='form-control col-12' type="number" placeholder='Day...' name='days' onChange={handleOnChange}/>
-          <div className='col-12 d-flex justify-content-center mt-4'>
-            <button className='btn custom-btn w-75' onClick={addHomeWork}>Add Homework</button>
+        <div className="container my-5 g-2 row align-items-center w-75">
+          <input
+            className="form-control col-12"
+            type="text"
+            placeholder="Homeworks..."
+            ref={newInputTodo}
+          />
+          <input
+            className="form-control col-12"
+            type="number"
+            placeholder="Day..."
+            ref={newInputTodo2}
+          />
+          <div className="col-12 d-flex justify-content-center mt-4">
+            <button className="btn custom-btn w-75" onClick={AddInputTodo}>
+              Add Homework
+            </button>
           </div>
         </div>
       </div>
-      <div className="todoList"></div>
-    </div>
+      <div className="todoList mt-5 container">
+        {/* {todoLists.map((todoList) => (
+          <div key={todoList.id}>
+            {todoList.homeWorkName}
+            {todoList.daysToComplete}
+            <button
+              onClick={() => dispatch({ type: "REMOVE", id: todoList.id })}
+            >
+              Remove
+            </button> */}
+
+            <ul className="ul-custom list-group list-group-numbered container-fluid my-5">
+              {todoLists.map((todoList) =>(
+                <li className="list-group-item d-flex justify-content-center align-items-center" key={todoList.id}>
+                  <div className="ms-4 me-auto d-flex flex-column align-items-start">
+                    <div className="fw-bold fs-5">{todoList.homeWorkName}</div>
+                    <div className='fs-6'>
+                    Day to Complete : {todoList.daysToComplete}
+                    </div>
+                  </div>
+                  <button
+                onClick={() => dispatch({ type: "REMOVE", id: todoList.id })}
+              >
+                Remove
+              </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+      </div>
+    // </div>
   );
 }
 
